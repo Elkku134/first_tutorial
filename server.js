c3e56167e20ffbd70ad1df386724b756276f4150
unknown <nikimalmsten@hotmail.com>
2024-11-14 19:40:00 +0200

    deleted the extra files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     //res.setHeader('Content-Type', 'text/html')
     //res.end('<h1>Hello oworld </h1>');
 
-    res.writeHead(500, { 'content-type': 'application/json'});
+    res.writeHead(500, { 'Content-type': 'application/json'});
     res.end(JSON.stringify({ message: 'Server Error'}));
 });
 
