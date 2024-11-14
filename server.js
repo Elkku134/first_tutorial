faab28b3d6a21924ed1ccf8948bb63ce03f8825e
unknown <nikimalmsten@hotmail.com>
2024-11-14 21:22:57 +0200

    updating all the files

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 import http from 'http';
-const PORT = 8000;
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+import { error } from 'console';
+const PORT = process.env.PORT;
+
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//console.log('Filename is:  ', __filename, 'Dirname is:  ', __dirname);
 
 
-const server = http.createServer((req, res) => {
-    //res.setHeader('Content-Type', 'text/html')
-    //res.end('<h1>Hello oworld </h1>');
+const server = http.createServer(async(req, res) => {
+    try {
+        // check if get request
+        if(req.method === 'GET') {
+            let filePath;
+            if (req.url === '/') {
+                    filePath = path.join(__dirname, 'public', 'index.html');
+                }
+            
+                else if(req.url === '/about') {
+                    filePath = path.join(__dirname, 'public', 'about.html');
+                } 
+            
+                else {
+                    throw new Error('Not Found');
+                }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        }
+            
 
-    res.writeHead(500, { 'Content-type': 'application/json'});
-    res.end(JSON.stringify({ message: 'Server Error'}));
+        else {
+            throw new Error('Method not allowed');
+        }
+    } 
+    
+    catch(error) {
+        res.writeHead(500, { 'Content-type': 'text/plain'});
+        res.end('Server Error'); 
+    }
+
+
+
 });
 
 server.listen(PORT, () => {
