69630886f0bb63bd3d01473e563f9159f3e6e771
unknown <nikimalmsten@hotmail.com>
2024-11-14 21:43:21 +0200

    Adding files

---
+++ server2.js
@@ -0,0 +1,27 @@
+import { createServer} from 'http';
+const PORT = process.env.PORT;
+
+const users = [
+    { id: 1, name: 'John doe' },
+    { id: 2, name: 'Niki Malmsten' },
+    { id: 3, name: 'Lullu Huttunen' },
+];
+
+const server= createServer((req, res) => {
+    if (req.url === '/api/users' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(users));
+        res.end();
+    }
+    else {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 404;
+        res.write(JSON.stringify({message: 'Route not found'}));
+        res.end();
+    }
+
+});
+
+server.listen(PORT, () => {
+    console.log(`server runnin on ${PORT}`)
+});
