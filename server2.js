5a4b89015c4307fafa82c31db0358c549f3d12c0
Elkku134 <nikimalmsten@hotmail.com>
2024-11-15 18:13:37 +0200

    did the api and cleaned it up

--- server2.js
+++ server2.js
@@ -7,19 +7,60 @@ const users = [
     { id: 3, name: 'Lullu Huttunen' },
 ];
 
-const server= createServer((req, res) => {
-    if (req.url === '/api/users' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(users));
-        res.end();
-    }
-    else {
-        res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 404;
-        res.write(JSON.stringify({message: 'Route not found'}));
-        res.end();
-    }
+// Logeer midelware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
 
+// JSON middleware
+const jsonMiddleware = (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+}
+
+// Route ahndler foir GET /api/users
+const getUserHandler = (req,res) => {
+    res.write(JSON.stringify(users));
+    res.end();
+}
+
+// Route handler for GET api/users/:id
+const getUserByIdHandler = (req, res) => {
+    const id = req.url.split('/')[3];
+    const user = users.find((user) => user.id === parseInt(id));
+
+    if(user){
+        res.write(JSON.stringify(user));
+    }
+    else { 
+        res.statusCode = 404;
+        res.write(JSON.stringify({message: 'User not found'}));
+    }
+    res.end();
+}
+
+// Not found handler
+const notFoundHandler = (req, res) => {
+    res.statusCode = 404;
+    res.write(JSON.stringify({message: 'Route not found'}));
+    res.end();
+}
+
+const server= createServer((req, res) => {
+    logger(req, res, () => {
+        jsonMiddleware(req, res, () => {
+            if(req.url === '/api/users' && req.method === 'GET'){
+                getUserHandler(req, res);
+            }
+            else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
+                getUserByIdHandler(req, res);
+            }
+            else {
+                notFoundHandler(req, res);
+            }
+        });
+    });
 });
 
 server.listen(PORT, () => {
